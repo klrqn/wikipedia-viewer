dd77d601c9d362b9a45e9700f7f43e8220d014cf
klrqn <burtonsb@gmail.com>
2018-06-04 01:14:45 +0000

    submit vars put together

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 window.onload = function() {
 
-    var searchTerm = document.getElementById("input").value;
+    $("#submit").on("click", function(e){
+        e.preventDefault(); //prevent reload
+
+        var searchTerm = document.getElementById("input").value;
+        console.log("search term:", searchTerm);
+
+
+    });
+
 
     var r = $.getJSON('https://en.wikipedia.org/w/api.php?action=opensearch&format=json&origin=*&limit=15&search=what');
 
